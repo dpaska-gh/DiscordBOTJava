3baa3155b9ada9f0f292ea3e972e55e95f606906
Mihael Rodek <mihael.rodek@fer.hr>
2020-12-10 09:53:34 +0100

    Added RiotStatus and tft command

--- src/main/java/discord/bot/Main.java
+++ src/main/java/discord/bot/Main.java
@@ -5,12 +5,14 @@ import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.javacord.api.entity.intent.Intent;
 
+import java.io.IOException;
+
 public class Main {
-    public static final String token = ApiKey.token;
+    public static final String token = "Nzc0NDA4OTI1MDkyNTExNzQ1.X6XWgw.8ISdCJvLOqLl4mWIohoxnRvIR0g";
 
     public static final DiscordApi api = new DiscordApiBuilder().setToken(token).setAllNonPrivilegedIntentsExcept(Intent.GUILD_WEBHOOKS).login().join();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         PongCommand pong = new PongCommand();
         pong.pongCommand();
         CatFactCommand cat = new CatFactCommand();
@@ -25,6 +27,12 @@ public class Main {
 
         AtMentionCommand mentionCommand = new AtMentionCommand();
         mentionCommand.onMessageCreate();
+
+        RiotStats riotStats = new RiotStats();
+        riotStats.getStats();
+
+        TftCommand tftCommand=new TftCommand();
+        tftCommand.getTft();
 
         //discord.bot.mimiEasterEgg ee = new discord.bot.mimiEasterEgg();
         //ee.discord.bot.mimiEasterEgg();
