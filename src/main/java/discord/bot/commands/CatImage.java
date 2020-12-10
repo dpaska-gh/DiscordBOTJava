2068f679bda972268d470fb0e54b3dea64904eb9
Mihael Rodek <mihael.rodek@fer.hr>
2020-12-10 05:17:14 +0100

    Promjenjena struktura

--- src/main/java/discord/bot/commands/CatImage.java
+++ src/main/java/discord/bot/commands/CatImage.java
@@ -1,14 +1,17 @@
+package discord.bot.commands;
+
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import discord.bot.Main;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 
-public class catImage extends Main{
+public class CatImage extends Main {
     public void getCatImg() {
         Main.api.addMessageCreateListener(event -> {
             if(event.getMessageContent().equalsIgnoreCase("!pus")) {
---
+++ src/main/java/discord/bot/commands/JoinBotCommand.java
@@ -0,0 +1,82 @@
+package discord.bot.commands;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
+import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import discord.bot.LavaplayerAudioSource;
+import discord.bot.Main;
+import org.javacord.api.audio.AudioSource;
+import org.javacord.api.entity.channel.ServerVoiceChannel;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
+import org.javacord.api.entity.server.Server;
+
+import java.awt.*;
+
+
+public class JoinBotCommand extends Main {
+
+    public void joinChannel() {
+        api.addMessageCreateListener(event -> {
+            if (event.getMessage().getContent().equalsIgnoreCase("krava mi je vusla")) {
+                ServerVoiceChannel channel;
+                Server server = api.getServerById("774334763653136384").get();
+                //channel = api.getServerVoiceChannelById("774367293492822056").get();
+                channel = event.getMessage().getAuthor().asUser().get().getConnectedVoiceChannel(server).get();
+                System.out.println(channel.getName());
+
+                channel.connect().thenAccept(audioConnection -> {
+                    AudioPlayerManager playerManager = new DefaultAudioPlayerManager();
+                    playerManager.registerSourceManager(new YoutubeAudioSourceManager());
+                    AudioPlayer player = playerManager.createPlayer();
+                    AudioSource source = new LavaplayerAudioSource(api, player);
+                    audioConnection.setAudioSource(source);
+                    playerManager.loadItem("https://www.youtube.com/watch?v=7Fk1tjPn_sc", new AudioLoadResultHandler() {
+                        @Override
+                        public void trackLoaded(AudioTrack track) {
+                            player.playTrack(track);
+                        }
+
+                        @Override
+                        public void playlistLoaded(AudioPlaylist playlist) {
+                            for (AudioTrack track : playlist.getTracks()) {
+                                player.playTrack(track);
+                            }
+                        }
+
+                        @Override
+                        public void noMatches() {
+
+                        }
+
+                        @Override
+                        public void loadFailed(FriendlyException exception) {
+
+                        }
+                    });
+                }).exceptionally(e -> {
+                    // Failed to connect to voice channel (no permissions?)
+                    e.printStackTrace();
+                    return null;
+                });
+
+                EmbedBuilder embed = new EmbedBuilder()
+                        .setTitle("Now playing")
+                        .setDescription("Zadruga krava mi je v detelju vusla")
+                        .setColor(Color.BLUE);
+                event.getChannel().sendMessage(embed);
+            }
+            if (event.getMessage().getContent().equalsIgnoreCase("odjebi")) {
+                event.getChannel().sendMessage("Odjebi ti.");
+
+
+            }
+
+
+        });
+    }
+}
