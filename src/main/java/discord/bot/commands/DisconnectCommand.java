7c4db0ab890c15d83dca01ceeec88368d788f06e
PaskaCode <david.paska45@gmail.com>
2020-12-11 12:41:38 +0100

    Added disconnect command

---
+++ src/main/java/discord/bot/commands/DisconnectCommand.java
@@ -0,0 +1,30 @@
+package discord.bot.commands;
+
+import discord.bot.commands.finals.FinalValues;
+import org.javacord.api.entity.server.Server;
+import org.javacord.api.event.message.MessageCreateEvent;
+
+import java.util.Collections;
+import java.util.List;
+
+public class DisconnectCommand implements TemplateCommand {
+    @Override
+    public void executeCommand(MessageCreateEvent event) {
+        Server server = event.getServer().get();
+        if (event.getMessage().getContent().equalsIgnoreCase(FinalValues.PREFIX + FinalValues.ODJEBI)) {
+            event.getChannel().sendMessage("Odjebi ti.");
+            server.getAudioConnection().get().close();
+
+        }
+    }
+
+    @Override
+    public String getCommandName() {
+        return FinalValues.ODJEBI;
+    }
+
+    @Override
+    public List<String> getCommandDescription() {
+        return Collections.singletonList("Disconnects the bot.");
+    }
+}
