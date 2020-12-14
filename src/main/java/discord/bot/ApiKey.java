ed21d5a6a790bc3a54ee4dce4ccd827c158721e2
PaskaCode <david.paska45@gmail.com>
2020-12-14 09:00:39 +0100

    Fixed skip command if skipping the last track, added Temp channel choose name functionality, improved bot disconnect on empty queue

--- src/main/java/discord/bot/ApiKey.java
+++ src/main/java/discord/bot/ApiKey.java
@@ -1,6 +1,6 @@
 package discord.bot;
 
 public class ApiKey {
-    public static final String token = "Nzc0NDA4OTI1MDkyNTExNzQ1.X6XWgw.u20Tcw1QDfpOdTtS7SlaPPZ6XHY";
-    public static final String riotApiKey = "RGAPI-2581256d-0e13-42dc-a975-c0f6aa1876b6";
+    public static final String token = "/";
+    public static final String riotApiKey = "/";
 }
