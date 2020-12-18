cd137f7324a5926a64e87e2dbec1e7d10e635a88
PaskaCode <david.paska45@gmail.com>
2020-12-18 10:00:47 +0100

    ClearCommand cleanup

--- src/main/java/discord/bot/commands/ClearCommand.java
+++ src/main/java/discord/bot/commands/ClearCommand.java
@@ -9,9 +9,14 @@ import java.util.List;
 public class ClearCommand implements TemplateCommand {
     @Override
     public void executeCommand(MessageCreateEvent event) {
-        if (event.getMessageContent().equalsIgnoreCase(FinalValues.PREFIX + FinalValues.CLEAR))
-            JoinBotCommand.trackScheduler.queue.clear();
-        event.getChannel().sendMessage("The queue has been cleared");
+        if (event.getMessageContent().equalsIgnoreCase(FinalValues.PREFIX + FinalValues.CLEAR)) {
+            if (JoinBotCommand.user.isConnected(JoinBotCommand.audioConnection.getChannel())) {
+                JoinBotCommand.trackScheduler.queue.clear();
+                event.getChannel().sendMessage("The queue has been cleared");
+            } else
+                event.getChannel().sendMessage("You have to be connected to a voice channel to use this command");
+
+        }
     }
 
     @Override
